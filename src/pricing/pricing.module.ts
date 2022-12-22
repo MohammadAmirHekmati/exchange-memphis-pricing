1f044f622e6ae2139daf0e6ac461a864c59548b3
Seyyed Mohammad Amir Hekmati <mohammadhekmati1153@gmail.com>
2022-12-22 00:14:24 -0800

    Update pricing.module.ts

--- src/pricing/pricing.module.ts
+++ src/pricing/pricing.module.ts
@@ -8,7 +8,7 @@ import { BotStableCoinService } from './bot.stable.coin.service';
 import { CryptoPricingService } from './crypto.pricing.service';
 
 @Module({
-    imports:[MemphisModule,GlobalModule,OriginRedisModule.forRoot("192.168.10.200",6389)],
+    imports:[MemphisModule,GlobalModule,OriginRedisModule.forRoot("192.168.10.200",6379)],
     providers:[BotManualCoinsService,BotIrrService,CryptoPricingService,BotStableCoinService]
     // BotIrrService,CryptoPricingService,BotStableCoinService,
 })
